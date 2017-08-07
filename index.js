234a5bd0b861f626ee887ae40b903a61722859c8
Edwin Brower <edwin.brower@gmail.com>
2017-08-07 15:25:26 -0700

    further setup of game logic including setting up functions and starting getinput

--- index.js
+++ index.js
@@ -3,15 +3,35 @@ let playerSwitch = true; // If true then player1. if false player2
 let movesLeft = 9;
 let gameWon = false;
 
-while (movesLeft && !gameWon) {
-  console.log(board);
+let checkForWin = () => {
+
+};
+
+let playMove = (input) => {
+  
+};
+
+let getInput = () => {
   if (playerSwitch) {
     console.log('Player 1 it is your turn');
-
   } else {
     console.log('Player 2 it is your turn');
-
   }
+  console.log('Enter "l", "m", or "r", to play the left, middle, or right spots');
+  let input = '';
+  while (input !== 'l' && input !== 'm' && input !== 'r') {
+    input = 'l';
+  }
+  console.log('You have played ' + input);
+  return input;
+};
+
+while (movesLeft && !gameWon) {
+  console.log(board);
+  let input = getInput();
+  playMove(input);
+  checkForWin();
+
   if (gameWon) {
     let winner = playerSwitch ? '1' : '2';
     console.log('Congrats! Player ' + winner + ' you have won!');
